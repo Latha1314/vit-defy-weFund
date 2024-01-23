695bc443f8284f85eb435d5b139cbf66b29f42f9
Latha <lathamallesh2@gmail.com>
2024-01-23 11:50:07 +0530

    created context for the app

---
+++ Backend/wefund/hardhat.config.js
@@ -0,0 +1,52 @@
+require("@matterlabs/hardhat-zksync-solc");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const key  = "8d5dc68f333ed9ca6b8482e78d21a69a1f50d861b839dbbddee0d3b5e49c8897"
+module.exports = {
+  zksolc: {
+    version: "1.3.9",
+    compilerSource: "binary",
+    settings: {
+      optimizer: {
+        enabled: true,
+      },
+    },
+  },
+  networks: {
+    zksync_testnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli",
+      chainId: 280,
+      zksync: true,
+    },
+    zksync_mainnet: {
+      url: "https://zksync2-mainnet.zksync.io/",
+      ethNetwork: "mainnet",
+      chainId: 324,
+      zksync: true,
+    },
+  },
+  paths: {
+    artifacts: "./artifacts-zk",
+    cache: "./cache-zk",
+    sources: "./contracts",
+    tests: "./test",
+  },
+  solidity: {
+    version: "0.8.17",
+    networks : {
+      hardhat:{},
+      polygon:{
+        url:'https://rpc.ankr.com/polygon_mumbai',
+        accounts: [`0x${KEY}`]
+      }
+    },
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
